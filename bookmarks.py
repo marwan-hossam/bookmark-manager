1e0a5a97fa21964696764b240b1c443e1dc3d30d
marwan-hossam <marwan.hoosam01@gmail.com>
2025-08-31 23:27:16 +0300

    bookmarks.py

--- bookmarks.py
+++ bookmarks.py
@@ -1,59 +1,3 @@
-# #--------------------------------
-# #----Simple Bookmark Manager-----
-# #--------------------------------
-
-# # Emty List to store bookmarks
-# bookmarks = []
-
-# # Maximum number of allowed bookmarks
-# max_bookmarks = 5
-
-# while max_bookmarks > 0:
-
-#     # take website name from user
-#     site = input("Enter website name (without https:)")
-#     # Add the website to the list 
-#     bookmarks.append(f"https://{site.strip().lower()}")
-
-#     # Decrease availble slots
-#     max_bookmarks -= 1
-#     print(f"Website added successfully, {max_bookmarks} slots left")
-#     print(f"Current bookmarks: {bookmarks}")
-
-# else:
-#     print("Bookmarks lists is full, You can't add more websites")
-
-# # Check if list is not emty
-# if len(bookmarks) > 0:
-
-#     # Sort the list alphabetically
-#     bookmarks.sort()
-#     index = 0
-#     print("\nYour bookmarks: ")
-
-#     while index < len(bookmarks):
-#         print(f"{index + 1}.{bookmarks[index]}")
-#         index += 1
-
-# # Ask user if they want to delete a bookmark
-# delete_choice = input("\nDo you want to delete a bookmark? (Yes/No)")
-
-# if delete_choice == "Yes":
-#     delete_index = int(input("Enter the number of bookmark to delete"))
-
-#     if 1 <= delete_index <= len(bookmarks):
-#         removed = bookmarks.pop(delete_index - 1)
-#         print(f"{removed} has been removed from your bookmarks")
-    
-#     else: 
-#         print("Invalid number! No bookmark deleted")
-    
-#     print(f"\nFinal Bookmarks: {bookmarks}")
-
-# else: 
-#     print("No bookmark deleted")
-
-
 #-------------------------------
 #----Simple Bookmark Manager----
 #-------------------------------
@@ -115,3 +59,4 @@ while True:       # Loop keep running until user exits
 
     else:
         print("Invalid Choice! Please choose between (1-4). ")
+
